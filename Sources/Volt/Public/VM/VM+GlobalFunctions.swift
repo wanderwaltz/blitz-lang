e33ca0450dce1dd614f1ae2eab907b3aba109275
Egor Chiglintsev <wanderw@yandex-team.ru>
2018-05-24 18:49:22 +0700

    generate VM's registerGlobalFuncX functions using Sourcery

--- Sources/Volt/Public/VM/VM+GlobalFunctions.swift
+++ Sources/Volt/Public/VM/VM+GlobalFunctions.swift
@@ -1,3 +1,8 @@
+// Generated using Sourcery 0.13.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+
 extension VM {
     public func defineGlobalFunc0<R>(named name: String, `func`: @escaping () -> R) throws {
         try defineGlobal(
@@ -19,7 +24,6 @@ extension VM {
             })
         )
     }
-
     public func defineGlobalFunc2<P0, P1, R>(named name: String, `func`: @escaping (P0, P1) -> R) throws {
         try defineGlobal(
             named: name,
@@ -30,10 +34,7 @@ extension VM {
             })
         )
     }
-
-    public func defineGlobalFunc3<P0, P1, P2, R>
-                                 (named name: String,
-                                  `func`: @escaping (P0, P1, P2) -> R) throws {
+    public func defineGlobalFunc3<P0, P1, P2, R>(named name: String, `func`: @escaping (P0, P1, P2) -> R) throws {
         try defineGlobal(
             named: name,
             value: AnyCallable({ _, args in
@@ -43,15 +44,72 @@ extension VM {
             })
         )
     }
-
-    public func defineGlobalFunc4<P0, P1, P2, P3, R>
-                                 (named name: String,
-                                  `func`: @escaping (P0, P1, P2, P3) -> R) throws {
+    public func defineGlobalFunc4<P0, P1, P2, P3, R>(named name: String, `func`: @escaping (P0, P1, P2, P3) -> R) throws {
         try defineGlobal(
             named: name,
             value: AnyCallable({ _, args in
                 return try typecheck(args, P0.self, P1.self, P2.self, P3.self) {
                     .init(`func`($0, $1, $2, $3))
+                }
+            })
+        )
+    }
+    public func defineGlobalFunc5<P0, P1, P2, P3, P4, R>(named name: String, `func`: @escaping (P0, P1, P2, P3, P4) -> R) throws {
+        try defineGlobal(
+            named: name,
+            value: AnyCallable({ _, args in
+                return try typecheck(args, P0.self, P1.self, P2.self, P3.self, P4.self) {
+                    .init(`func`($0, $1, $2, $3, $4))
+                }
+            })
+        )
+    }
+    public func defineGlobalFunc6<P0, P1, P2, P3, P4, P5, R>(named name: String, `func`: @escaping (P0, P1, P2, P3, P4, P5) -> R) throws {
+        try defineGlobal(
+            named: name,
+            value: AnyCallable({ _, args in
+                return try typecheck(args, P0.self, P1.self, P2.self, P3.self, P4.self, P5.self) {
+                    .init(`func`($0, $1, $2, $3, $4, $5))
+                }
+            })
+        )
+    }
+    public func defineGlobalFunc7<P0, P1, P2, P3, P4, P5, P6, R>(named name: String, `func`: @escaping (P0, P1, P2, P3, P4, P5, P6) -> R) throws {
+        try defineGlobal(
+            named: name,
+            value: AnyCallable({ _, args in
+                return try typecheck(args, P0.self, P1.self, P2.self, P3.self, P4.self, P5.self, P6.self) {
+                    .init(`func`($0, $1, $2, $3, $4, $5, $6))
+                }
+            })
+        )
+    }
+    public func defineGlobalFunc8<P0, P1, P2, P3, P4, P5, P6, P7, R>(named name: String, `func`: @escaping (P0, P1, P2, P3, P4, P5, P6, P7) -> R) throws {
+        try defineGlobal(
+            named: name,
+            value: AnyCallable({ _, args in
+                return try typecheck(args, P0.self, P1.self, P2.self, P3.self, P4.self, P5.self, P6.self, P7.self) {
+                    .init(`func`($0, $1, $2, $3, $4, $5, $6, $7))
+                }
+            })
+        )
+    }
+    public func defineGlobalFunc9<P0, P1, P2, P3, P4, P5, P6, P7, P8, R>(named name: String, `func`: @escaping (P0, P1, P2, P3, P4, P5, P6, P7, P8) -> R) throws {
+        try defineGlobal(
+            named: name,
+            value: AnyCallable({ _, args in
+                return try typecheck(args, P0.self, P1.self, P2.self, P3.self, P4.self, P5.self, P6.self, P7.self, P8.self) {
+                    .init(`func`($0, $1, $2, $3, $4, $5, $6, $7, $8))
+                }
+            })
+        )
+    }
+    public func defineGlobalFunc10<P0, P1, P2, P3, P4, P5, P6, P7, P8, P9, R>(named name: String, `func`: @escaping (P0, P1, P2, P3, P4, P5, P6, P7, P8, P9) -> R) throws {
+        try defineGlobal(
+            named: name,
+            value: AnyCallable({ _, args in
+                return try typecheck(args, P0.self, P1.self, P2.self, P3.self, P4.self, P5.self, P6.self, P7.self, P8.self, P9.self) {
+                    .init(`func`($0, $1, $2, $3, $4, $5, $6, $7, $8, $9))
                 }
             })
         )
