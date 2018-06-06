f87f6bef748391e4cc41c1eaff4f5e54169ee859
Egor Chiglintsev <wanderw@yandex-team.ru>
2018-06-06 10:02:02 +0700

    add reverse value convertible tests for Bool

--- Sources/Volt/Internal/Interpreter/Result/Value+Interpreter.swift
+++ Sources/Volt/Internal/Interpreter/Result/Value+Interpreter.swift
@@ -5,7 +5,7 @@ extension Value {
         case let .bool(value): return value
         case let .number(value): return value != 0
         case let .string(value): return !value.isEmpty
-        case .array: return true
+        case let .array(array): return !array.isEmpty
         case .object: return true
         }
     }
