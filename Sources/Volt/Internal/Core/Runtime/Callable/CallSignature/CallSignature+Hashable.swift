27ebb02659dbac2462fdd1bd4bbf14eae0265b02
Egor Chiglintsev <wanderw@yandex-team.ru>
2018-06-08 15:17:27 +0700

    make CallSignature Hashable

--- Sources/Volt/Internal/Core/Runtime/Callable/CallSignature/CallSignature+Hashable.swift
+++ Sources/Volt/Internal/Core/Runtime/Callable/CallSignature/CallSignature+Hashable.swift
@@ -1,4 +1,8 @@
-extension CallSignature: Equatable {
+extension CallSignature: Hashable {
+    var hashValue: Int {
+        return components.map({ $0.hashValue }).reduce(0, ^)
+    }
+
     static func == (left: CallSignature, right: CallSignature) -> Bool {
         return left.components == right.components
     }
